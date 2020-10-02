2948224eff6bc69e6a3640ba4304dcb3c8223591
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-10-01 22:48:34 -0300

    Implementacao do manifest.js e criacao dos icons do projeto PWA

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
 const cacheName = 'app-shell-v1';
 const assetsToCache = [
-  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-  'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
-  'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
-  'https://fonts.googleapis.com/css?family=Roboto:400,700',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://unpkg.com/dexie@latest/dist/dexie.min.js',
   './assets/icons/agenda.jpg',
   './assets/icons/favicon.png',
